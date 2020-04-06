1b830e05d94a2b89d94fd4909038bfb5f97dd824
Carlos Lenon <code@carloslenon.com>
2020-04-05 21:15:24 -0600

    add mongoose dependencie add babel core transpiler

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @file index.js
+ * @class Base File
+ * @classdesc Interface to comunicate with the API.
+ * @since v1.0.0
+ * @author boykland/clenondavis <clenondavis@outlook.com>
+ */
+require("@babel/register");
+require("@babel/polyfill");
+require("./server.js");
