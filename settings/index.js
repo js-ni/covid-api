3ab7db3223ddea2cf6934433680fb9975d5af799
Carlos Lenon <code@carloslenon.com>
2020-04-06 15:07:15 -0600

    Feature/implement mongoose configuration (#3)
    * add mongoose dependencie
    add babel core transpiler
    
    * add babel config file
    add gitignoore file
    add models and schema base
    add nodemon config
    add file for service, setting and rest for base config service
    
    * fix issue from untracked file and gitignore file config
    
    * add mongoose implementation
    add service and base config
    
    * ignore pachage lock

---
+++ settings/index.js
@@ -0,0 +1,11 @@
+/**
+ * @file rest/settings/index.js
+ * @module Settings
+ * @desc This is the encapsulation for all settings context
+ * @since v1.0
+ * @author @clenondavis <dev@carloslenon.com>
+ * @return {Object} List of settings
+ */
+
+import * as routes from "./routes";
+export { routes };
